c23ca5cce0e554baaf13f5058e5b356d17f2bfcb
Gabriel Aragão <gabrielaragaofds@gmail.com>
2019-08-16 16:01:09 -0300

    Vs 0.0.0 base

---
+++ Assets/Scripts/Behaviours/PlayerBehaviour_1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour_1 : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rbPlayer;
+    [SerializeField] private float speed;
+    private float velocity_x;
+    
+    void Start()
+    {
+        speed = 4;
+    }
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        Moviment();
+    }
+    void Moviment(){
+        velocity_x = Input.GetAxisRaw("Horizontal");   
+        rbPlayer.velocity = new Vector2(velocity_x * speed,rbPlayer.velocity.y);
+    }
+}
