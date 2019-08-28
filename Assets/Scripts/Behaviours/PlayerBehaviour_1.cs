80b7b64755f336c9999feaf15731bcb62f316c4c
Aluno <gabrieldias2032@gmail.com>
2019-08-28 12:01:55 -0300

    vs 0.0.9 reflectShield add

--- Assets/Scripts/Behaviours/PlayerBehaviour_1.cs
+++ Assets/Scripts/Behaviours/PlayerBehaviour_1.cs
@@ -8,18 +8,20 @@ public class PlayerBehaviour_1 : MonoBehaviour
 {
     
     [SerializeField] private Rigidbody2D rbPlayer;
-    [SerializeField] private GameObject InterfaceCanvas;
+    [SerializeField] private GameObject InterfaceCanvas, reflectShield, clone;
     [SerializeField] private AudioClip[] sound;
-    [SerializeField] private float speed, forceJump, radius, groundTestP,wallTestP,wallTestD,wallTestDY,time; //P = position; D = distance
+    [SerializeField] private float speed, forceJump, radius, groundTestP,wallTestP,wallTestD,wallTestDY; //P = position; D = distance
+    [SerializeField] private float mana, manaCount;
     private float velocity_x;
     private AudioSource outPutSound;
     private Collider2D[] olReturn;
-    private bool inGround, frontRight;
+    private bool inGround, frontRight, execute, moviment;
     private RaycastHit2D wallTest;
     private UI ui;
    
     void Start()
     {
+        moviment = true;
         outPutSound = GetComponent<AudioSource>();
         outPutSound.volume = 0.008f;
         ui = InterfaceCanvas.GetComponent<UI>();
@@ -30,14 +32,16 @@ public class PlayerBehaviour_1 : MonoBehaviour
         ui.Fades(true,1,Random.Range(1,4));
     }
      void Update()
-    {
-        
+    {  
         inGround = GroundTest();
         Jump();
+        ChargeMana();
+        ReflectShield();
     }
     void FixedUpdate()
     {
-        Moviment();
+        if(moviment)
+            Moviment();
     }
     void Moviment(){
         velocity_x = Input.GetAxisRaw("Horizontal");   
@@ -67,10 +71,43 @@ public class PlayerBehaviour_1 : MonoBehaviour
         }
         return false;
     }
-
+    void ChargeMana()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine("ManaCount");
+            moviment = false;
+            rbPlayer.velocity = Vector2.zero;
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+        {
+            execute = true;
+            moviment = true;
+        }
+    }
+    void ReflectShield()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            clone = Instantiate(reflectShield, transform.position + new Vector3(0.5f, 0), Quaternion.identity,gameObject.transform);
+        }
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            Destroy(clone);
+        }
+    }
     private void OnDrawGizmos()
     {
         //Debug.DrawLine(transform.position + new Vector3(0.5f,-0.28f), transform.position + new Vector3(0.5f,0.28f),new Color(1,1,1));
         Gizmos.DrawWireSphere(new Vector2(transform.position.x, transform.position.y - groundTestP), radius);
+    }
+    IEnumerator ManaCount()
+    {
+        if (execute)
+        {
+            manaCount = Time.time;
+            execute = false;
+        }
+        yield return mana = manaCount - Time.time;
     }
 }
